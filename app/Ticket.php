90be41bf8800d74a6a2d2935ea9a09d58dd4b37f
LaiDinhTu <tuvbhp1807@gmail.com>
2020-07-02 12:20:41 +0700

    Menu_Ticket

---
+++ app/Ticket.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Ticket extends Model
+{
+    protected $fillable = ['title', 'content', 'slug', 'status', 'user_id'];
+    protected $guarded = ['id'];
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    public function getTitle()
+    {
+        return $this->title;
+}
+
+}
---
+++ resources/views/project/home.blade.php
@@ -0,0 +1,11 @@
+@extends('project.master')
+@section('title', 'TRANG CHU')
+
+@section('content')
+    <div class="container">
+        <div class="content" align="center">
+            <div class="title">Home Page</div>
+            <div class="quote">Our Home page!</div>
+        </div>
+    </div>
+@endsection
